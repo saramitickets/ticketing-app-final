3140c05cb7c7b98601168710a6b0c1bcf61dd2d6
saramitickets <sarami.events254@gmail.com>
2025-07-01 15:01:17 +0300

    Update server.js

--- server.js
+++ server.js
@@ -161,7 +161,7 @@ app.post('/api/create-order', async (req, res) => {
             customerPhone: cleanedPhoneNumber, // Customer's phone number for STK Push
             transactionReference: orderRef.id, // Use Firestore order ID as transaction reference
             callbackURL: process.env.YOUR_APP_CALLBACK_URL ,// URL where InfinitiPay sends status updates
-            "ptyId": "1"
+            ptyId: "1"
         };
 
         console.log('Sending STK Push request with payload:', JSON.stringify(stkPushPayload, null, 2));
